78a724b2a0819090a5eeaebdc4327741ce0974b8
Bozhidar Topchiyski <bozhidar.topchiyski@sap.com>
2024-12-10 23:54:24 +0200

    Train and compare multiple regression models

---
+++ tomorrow/script/ml/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.0.0"
+__date__ = "05-12-24"
+__author__ = "B.Topchiyski"
+
+MODEL_DIR = "tomorrow/ml/model"
+SCALER_DIR = "tomorrow/ml/scaler"
---
+++ tomorrow/script/ml/preprocessing.py
@@ -0,0 +1,38 @@
+"""Handles all data preparation tasks like reading the dataset, splitting the data, and scaling"""
+
+import os
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
+
+from tomorrow.script.ml import SCALER_DIR
+
+def load_data(file_path):
+    """Load data from a CSV file."""
+    return pd.read_csv(file_path)
+
+def split_data(data, features, targets, test_size=0.3, random_state=0):
+    """Split data into train and test sets."""
+    x = data[features]
+    y = data[targets]
+    return train_test_split(x, y, test_size=test_size, random_state=random_state)
+
+def scale_data(x_train, x_test, y_train, y_test):
+    """Scale the data using RobustScaler and save the scalers."""
+    scaler_x = RobustScaler()
+    scaler_x.fit(x_train)
+    x_train_scaled = scaler_x.transform(x_train)
+    x_test_scaled = scaler_x.transform(x_test)
+
+    scaler_y = RobustScaler()
+    scaler_y.fit(y_train)
+    y_train_scaled = scaler_y.transform(y_train)
+    y_test_scaled = scaler_y.transform(y_test)
+
+    # Save scalers
+    joblib.dump(scaler_x, os.path.join(SCALER_DIR, "scaler_x.joblib"))
+    joblib.dump(scaler_y, os.path.join(SCALER_DIR, "scaler_y.joblib"))
+
+    return x_train_scaled, x_test_scaled, y_train_scaled, y_test_scaled
